27df9cbad033247b35d5e85704f63538fdbb94c2
Adi Sanjaya <putuadisanjaya04112000@gmail.com>
2022-04-12 10:46:18 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'da2fe65540f320333d05fc20967b0c39'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '770b91fa-90e5-490c-ae83-8710881569f2'
+};
